8ee0cb23eedefd3409d680e7c8c7143c9253f66d
Anders Fischer-Nielsen <andersfischern@me.com>
2015-09-28 11:38:49 +0200

    Changed to dup2 (it closes automatically). Commented more.

--- ObligatoriskOpgave1/bosh/bosh.c
+++ ObligatoriskOpgave1/bosh/bosh.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
 #include "parser.h"
 #include "print.h"
 
@@ -44,30 +45,26 @@ Cmd* reverse(Cmd* root) {
 void startChild(Cmd *command, int readPipe, int writePipe, int first) {
     signal(SIGINT, SIG_DFL);                        //Enable CTRL+C to exit a program.
 
-    if (command->next) {							//First or several commands.
+    if (command->next) {							//This is one of several commands.
         if (first) {
             close(readPipe);						//Close the pipe end we don't need.
-            close(STDOUT_FILENUMBER);               //Substitute stdout with pipe.
-            dup(writePipe);
+            dup2(writePipe, STDOUT_FILENUMBER);     //Substitute stdout with pipe.
             close(writePipe);						//Close after "assigning" pipe end.
         }
-        else {                                      //If this is not the first command.
-            close(STDIN_FILENUMBER);				//Replace stdin and stdout with pipe ends.
-            dup(readPipe);
+        else {                                      //"Middle" command.
+            dup2(readPipe, STDIN_FILENUMBER);
             close(readPipe);
 
-            close(STDOUT_FILENUMBER);
-            dup(writePipe);
+            dup2(writePipe, STDOUT_FILENUMBER);
             close(writePipe);
         }
     }
-    else {											//Last or single command.
+    else {											//This is the last or a single command.
         if (!first) {                               //This is the last command.
             close(writePipe);
-            close(STDIN_FILENUMBER);                //Substitute stdin as above, now just stdin.
-            dup(readPipe);
+            dup2(readPipe, STDIN_FILENUMBER);       //Substitute stdin as above, now just stdin.
             close(readPipe);
-        } else {
+        } else {                                    //Single command.
             close(readPipe);
             close(writePipe);
         }
@@ -75,7 +72,7 @@ void startChild(Cmd *command, int readPipe, int writePipe, int first) {
 
     char** command_and_parameters = command->cmd;                   	//Extract the first command from the user input.
     execvp(command_and_parameters[0], command_and_parameters);          //Execute.
-    
+
     printf("%s: Command not found.\n", command_and_parameters[0]);      //If these lines of code are run, it means command not found.
     exit(-1);                                                           //Exit with error state.
 }
@@ -85,10 +82,10 @@ int executeshellcmd (Shellcmd *shellcmd)
 { 
     int pipe_ends[2];
     pid_t process_id;
-    Cmd *current_cmd = reverse(shellcmd->the_cmds);
-    int first = 1;
+    Cmd *current_cmd = reverse(shellcmd->the_cmds); //Commands are parsed back-to-front, so we reverse.
     
-    pipe(pipe_ends);
+    int first = 1;                              //We're at the first command at first run.
+    if (pipe(pipe_ends) == -1) return -1;
 
     while (current_cmd) {
         process_id = fork();                   	//Instantiate new process.
@@ -96,18 +93,21 @@ int executeshellcmd (Shellcmd *shellcmd)
         if (process_id == 0) {                 	//This is a child process.
             startChild(current_cmd, pipe_ends[0], pipe_ends[1], first);
         }
-        else {                                 	//Parent process.
-            first = 0;
-            current_cmd = current_cmd->next;
-        }
+        
+        //If we reach this, then this is the parent process.
+        first = 0;                              //No longer the first command.
+        current_cmd = current_cmd->next;        //Assign the next command for execution.
     }
-    close(pipe_ends[STDIN_FILENUMBER]);                      	//Close the pipe in parent
+    
+    close(pipe_ends[STDIN_FILENUMBER]);         //Close the pipe in parent
     close(pipe_ends[STDOUT_FILENUMBER]);
 
-    if (!shellcmd->background) {                //If the user specified to execute in background, then don't wait.
-        int exit_code;
-        waitpid(process_id, &exit_code, 0);     //Assert that the process executed succesfully.
+    if (shellcmd->background) {                 //If the user specified to execute in background, then don't wait.
+        return 0;
     }
+    
+    int exit_code;
+    waitpid(process_id, &exit_code, 0);         //Assert that the process executed succesfully.
 
     return 0;
 }
