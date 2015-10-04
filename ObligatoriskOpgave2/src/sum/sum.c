eb64b7cf52f059d5fce3468d0a1f545a2914ff9a
Anders Fischer-Nielsen <andersfischern@me.com>
2015-10-04 12:01:18 +0200

    Made example from book compile properly.

--- ObligatoriskOpgave2/src/sum/sum.c
+++ ObligatoriskOpgave2/src/sum/sum.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <math.h>
+#include <stdlib.h>
 
-int sum; /* this data is shared by the thread(s) */
+float sum; /* this data is shared by the thread(s) */
 void *runner(void *param); /* threads call this function */
 
 int main(int argc, char* argv[]) {
-	pthread t tid; //Thread identifier.
-	pthread attr t attr; //Thread attributes.
+	pthread_t tid; //Thread identifier.
+	pthread_attr_t attr; //Thread attributes.
 
 	//Get the default attributes.
-	pthread attr init(&attr);
+	pthread_attr_init(&attr);
 
 	//Create the thread.
-	pthread create(&tid, &attr, runner, argv[1]); //Wait for the thread to exit.
-	pthread join(tid, NULL); 
+	pthread_create(&tid, &attr, runner, argv[1]); //Wait for the thread to exit.
+	pthread_join(tid, NULL); 
 
-	printf("sum = %d\n", sum);
+	printf("sum = %f \n", sum);
 
 	return 0;
 }
@@ -23,11 +25,12 @@ int main(int argc, char* argv[]) {
 //Function to give to thread to execute.
 void *runner(void *param)
 {
- 	int i, upper = atoi(param);
+ 	int i;
+ 	int upper = atoi(param);
  	
  	sum = 0;
  	for (i = 1; i <= upper; i++) {
-       sum += i;
+       sum += sqrt(i);
  	}
 
 	pthread_exit(0);
