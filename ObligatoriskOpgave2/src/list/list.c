f83e616c0e4dcc60d2bc83cdf4fa54ee990cbbe2
Anders Wind <wind@airtame.com>
2015-10-13 23:41:48 +0200

    Removed initiators and added locks on all methods

--- ObligatoriskOpgave2/src/list/list.c
+++ ObligatoriskOpgave2/src/list/list.c
@@ -11,14 +11,16 @@
 #include <pthread.h>
 #include "list.h"
 
-int lock = 0;
+int lock;
 
-pthread_mutex_t mutex = 0;
+pthread_mutex_t mutex;
 
 
 /* list_new: return a new list structure */
 List *list_new(void)
 {
+  // mutex
+  pthread_mutex_lock(&mutex);
   List *l;
 
   l = (List *) malloc(sizeof(List));
@@ -28,6 +30,7 @@ List *list_new(void)
   l->first = l->last = (Node *) malloc(sizeof(Node));
   l->first->elm = NULL;
   l->first->next = NULL;
+  pthread_mutex_unlock(&mutex);
   return l;
 }
 
@@ -58,6 +61,8 @@ void list_add(List *l, Node *n)
 /* list_remove: remove and return the first (non-root) element from list l */
 Node *list_remove(List *l)
 {
+  // mutex
+  pthread_mutex_lock(&mutex);
   if(l->len == 0) return NULL;
   
   // // lock 1
@@ -69,9 +74,6 @@ Node *list_remove(List *l)
   // while(key == 1) {
   //   swap(&lock, &key);
   // }
-  
-  // mutex
-  pthread_mutex_lock(&mutex);
   
   Node *n = l->first->next;
   l->first->next = l->first->next->next;
@@ -85,21 +87,29 @@ Node *list_remove(List *l)
 /* node_new: return a new node structure */
 Node *node_new(void)
 {
+  // mutex
+  pthread_mutex_lock(&mutex);
   Node *n;
+  
   n = (Node *) malloc(sizeof(Node));
   n->elm = NULL;
   n->next = NULL;
+  
+  pthread_mutex_unlock(&mutex);
   return n;
 }
 
 /* node_new_str: return a new node structure, where elm points to new copy of s */
 Node *node_new_str(char *s)
 {
+  // mutex
+  pthread_mutex_lock(&mutex);
   Node *n;
   n = (Node *) malloc(sizeof(Node));
   n->elm = (void *) malloc((strlen(s)+1) * sizeof(char));
   strcpy((char *) n->elm, s);
   n->next = NULL;
+  pthread_mutex_unlock(&mutex);
   return n;
 }
 
