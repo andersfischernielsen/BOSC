75230c9a6b536b356921e0174f72e5c9671526ff
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2015-10-16 17:53:05 +0200

    Use structs to separate work in producer_consumer

--- ObligatoriskOpgave2/src/producer_consumer/producer_consumer.c
+++ ObligatoriskOpgave2/src/producer_consumer/producer_consumer.c
@@ -9,15 +9,19 @@ void *consumer(void *param);
 void *producer(void *param);
 
 List *buf;
-int item_number;
+int buffer_size;
 sem_t empty, full;
 
-void fill_arr(int *arr, int size) {
-	int i;
-	for (i = 0; i < size; i++) {
-		arr[i] = i;
-	}
-}
+typedef struct {
+	int id;
+	int from;
+	int to;
+} prod_t;
+
+typedef struct {
+	int id;
+	int amount;
+} cons_t;
 
 int main(int argc, char* argv[]) {
 	if (argc != 5) {
@@ -27,37 +31,47 @@ int main(int argc, char* argv[]) {
 
 	int producer_number = atoi(argv[1]);
 	int consumer_number = atoi(argv[2]);
-	int buffer_size = atoi(argv[3]);
-	item_number = atoi(argv[4]);
+	buffer_size = atoi(argv[3]);
+	int item_number = atoi(argv[4]);
 
 	if (item_number == 0 || producer_number == 0 || consumer_number == 0 || item_number == 0) {
 		return 0;
 	}
 
 	buf = list_new();
-	
+
 	sem_init(&empty, 0, buffer_size);
 	sem_init(&full, 0, 0);
 
 	pthread_t producers[producer_number];
 	pthread_t consumers[consumer_number];
 
-	int p_arr[producer_number];
-	int c_arr[consumer_number];
-
-	fill_arr(p_arr, producer_number);
-	fill_arr(c_arr, consumer_number);
+	prod_t prod_work[producer_number];
+	cons_t cons_work[consumer_number];
 
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 
 	int i;
 	for (i = 0; i < producer_number; i++) {
-		pthread_create(producers + i, &attr, &producer, &p_arr[i]);
+		prod_work[i].id = i;
+		prod_work[i].from = i * (item_number / producer_number);
+		if (i == producer_number - 1) {
+			prod_work[i].to = item_number;
+		} else {
+			prod_work[i].to = (i+1) * (item_number / producer_number) - 1;
+		}
+		pthread_create(producers + i, &attr, &producer, prod_work + i);
 	}
 
 	for (i = 0; i < consumer_number; i++) {
-		pthread_create(consumers + i, &attr, &consumer, &c_arr[i]);
+		cons_work[i].id = i;
+		if (i == consumer_number - 1) {
+			cons_work[i].amount = item_number / consumer_number + item_number % consumer_number;
+		} else {
+			cons_work[i].amount = item_number / consumer_number;
+		}
+		pthread_create(consumers + i, &attr, &consumer, cons_work + i);
 	}
 
 	// Wait for threads.
@@ -74,60 +88,50 @@ int main(int argc, char* argv[]) {
 	printf("Consumers done\n");
 
 	// Free list buffer?
+	free(buf);
 	sem_destroy(&empty);
 	sem_destroy(&full);
-	
+
 	return 0;
 }
 
-int item_no = 0;
-
 void *producer(void *param) {
-	int id = *((int*) param);
-	int item;
+	prod_t work = *((prod_t*) param);
 	char itemstr[100];
-	while (item_number-- > 0) {
-		sprintf(itemstr, "node number: %d", item_no++);
-
+	int i;
+	for (i = work.from; i < work.to; i++) {
 		sem_wait(&empty);
+		sprintf(itemstr, "item: %d", i);
 		Node *n = node_new_str(itemstr);
 		list_add(buf, n);
 		int len = buf->len;
 
+		printf("Producer %d produced %s. Items in buffer: %d (Max: %d)\n", work.id, itemstr, len, buffer_size);
+
 		sem_post(&full);
-
-		printf("Producer %d produced %s. Items in buffer: %d\n", id, itemstr, len);
-
 		/* sleep for a random period of time */
 		Sleep(200);
 	}
-	printf("Producer %d stopped\n", id);
 	pthread_exit(NULL);
 }
 
 void *consumer(void *param) {
-	int id = *((int*) param);
-	int item;
-	while (buf->len != 0 || item_number > 0) {
-		if (buf->len != 0) {
-			sem_wait(&full);
+	cons_t work = *((cons_t*) param);
+	int i;
+	for (i = 0; i < work.amount; i++) {
+		sem_wait(&full);
 
-			Node *n = list_remove(buf);
-			if (n == NULL) {
-				printf("Null Node found!!!\n");
-				return NULL;
-			}
-			int len = buf->len;
-		
-			sem_post(&empty);
+		Node *n = list_remove(buf);
+		int len = buf->len;
 
-			printf("\tConsumer %d consumed %s. Items in buffer: %d\n", id, (char *)n->elm, len);
-			free(n->elm);
-			free(n);
-		}
+		printf("\tConsumer %d consumed %s. Items in buffer: %d\n", work.id, (char *)n->elm, len);
+		free(n->elm);
+		free(n);
+
+		sem_post(&empty);
+
 		/* sleep for a random period of time */
 		Sleep(200);
 	}
-	printf("Consumer %d stopped\n", id);
 	pthread_exit(NULL);
 }
