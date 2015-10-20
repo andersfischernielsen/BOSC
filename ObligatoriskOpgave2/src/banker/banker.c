68e69de605b3a21a72c4725ad110bc046c0f4143
Mikael Lindemann Jepsen <mikaellindemannjepsen@gmail.com>
2015-10-20 20:04:54 +0200

    Alternative implementation of banker

--- ObligatoriskOpgave2/src/banker/banker.c
+++ ObligatoriskOpgave2/src/banker/banker.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
 #include "../producer_consumer/sleep.h"
@@ -44,74 +45,99 @@ void print_array(int *toPrint, int length) {
 	printf("} \n");
 }
 
+int less_eq_int_arr(int *a, int *b, int length) {
+	int i;
+	for(i = 0; i < length; i++) {
+		if (a[i] > b[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int is_state_safe(State *state) {
+	// Step 1
+	int finish[m];
+
+	int i;
+
+	for (i = 0; i < m; i++) {
+		finish[i] = 0;
+	}
+
+	int work[n];
+
+	memcpy(work, state->available, n * sizeof(int));
+
+	// Step 2
+	int j;
+	for (i = 0; i < m; i++) {
+		if (!finish[i] && less_eq_int_arr(state->need[i], work, n)) {
+			// Step 3
+			for (j = 0; j < n; j++) {
+				work[j] += state->allocation[i][j];
+			}
+			finish[i] = 1;
+			i = -1; // Check all again.
+		}
+	}
+
+	// Step 4
+	for (i = 0; i < n; i++) {
+		if (!finish[i]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
 /* Allocate resources in request for process i, only if it 
    results in a safe state and return 1, else return 0 */
 int resource_request(int i, int *request) {
 	pthread_mutex_lock(&state_mutex);
-	printf("%s", "Request array: \n");
-	print_array(s->available, n);
-	print_array(request, n);
-	printf("%s", "STEP 1: Can the request be granted?\n");
-	//1. Can the request be granted? //
-	int j;
+
+	// Step 1 in book
 	// Iterate through the request.
-	for (j = 0; j < n; j++) {
-		int available = s->available[j] - request[j];
-		//If the request exceeds the available resources, the request cannot be fulfilled.
-		if (available < 0) {
-			//Return unsafe state.
-			pthread_mutex_unlock(&state_mutex);
-			return 0;
-		}
+	if (!less_eq_int_arr(request, s->need[i], n)) {
+		printf("Process %d has requested too much!\nRequest: ", i);
+		print_array(request, n);
+		printf("Need: ");
+		print_array(s->need[i], n);
+		// Should probably stop the process instead.
+		//Return unsafe state.
+		pthread_mutex_unlock(&state_mutex);
+		return 0;
 	}
 
-	//Clone the current state to check whether it's possible
-	State *cloned = allocate_state();
-	cpy_state(cloned, s);
-
-	printf("%s", "STEP 2: Assume that the request is granted.\n");
-	//2. Assume that the request is granted. //
-	for (j = 0; j < n; j++) {
-		printf("cloned->allocation before alloc: \n");
-		print_array(cloned->allocation[i], n);
-		//Allocate resources.
-		cloned->allocation[i][j] = cloned->allocation[i][j] + request[j];
-		printf("cloned->allocation after alloc: \n");
-		print_array(cloned->allocation[i], n);
-
-		printf("cloned->available before update:\n");
-		print_array(cloned->available, n);
-		//Update available resources counter.
-		cloned->available[j] = cloned->available[j] - request[j];
-		printf("cloned->available after update: \n");
-		print_array(cloned->available, n);
-
-		//Check that the allocation does not exceed max resources.
-		//If so, return unsafe state.
-		if (cloned->allocation[i][j] > cloned->max[i][j]) {
-			pthread_mutex_unlock(&state_mutex);
-			free_state(cloned);
-			return 0;
-		}
+	// Step 2 in book
+	if (!less_eq_int_arr(request, s->available, n)) {
+		//Return unsafe state.
+		pthread_mutex_unlock(&state_mutex);
+		return 0;
 	}
 
-	printf("%s", "STEP 3: Is the new state safe?\n");
-	//3. Is the new state safe? //
+	// Step 3 in book
+	int j;
 	for (j = 0; j < n; j++) {
-		//Calculate need matrix.
-		cloned->need[i][j] = cloned->max[i][j] - cloned->allocation[i][j];
-		//Check that the needed resources for each process
-		//doesn't exceed the available resources. If it does, unsafe state.
-		if (cloned->need[i][j] > cloned->available[j]) {
-			pthread_mutex_unlock(&state_mutex);
-			free_state(cloned);
-			return 0;
-		}
+		s->available[j] -= request[j];
+		s->allocation[i][j] += request[j];
+		s->need[i][j] -= request[j];
 	}
 
+	if (!is_state_safe(s)) {
+		for (j = 0; j < n; j++) {
+			s->available[j] += request[j];
+			s->allocation[i][j] -= request[j];
+			s->need[i][j] += request[j];
+		}
+
+		//Return unsafe state.
+		pthread_mutex_unlock(&state_mutex);
+		return 0;
+	}
 	//Else return safe state. Make s point to the correct "test state".
-	free_state(s);
-	s = cloned;
+	printf("Available after allocation of request %d: ", i);
+	print_array(s->available, n);
 	pthread_mutex_unlock(&state_mutex);
 	return 1;
 }
@@ -120,7 +146,7 @@ int resource_request(int i, int *request) {
 void resource_release(int i, int *request) {
 	pthread_mutex_lock(&state_mutex);
 	int j;
-	for (j = 0; j < m; j++) {
+	for (j = 0; j < n; j++) {
 		//Update allocated resources.
 		s->allocation[i][j] -= request[j];
 		//Update available resources counter.
@@ -131,44 +157,49 @@ void resource_release(int i, int *request) {
 
 /* Generate a request vector */
 void generate_request(int i, int *request) {
-	pthread_mutex_lock(&state_mutex);
 	int j, sum = 0;
 	while (!sum) {
-		for (j = 0; j < n; j++) {
-			request[j] = s->need[i][j] * ((double) rand()) / (double) RAND_MAX;
+		for (j = 0;j < n; j++) {
+			request[j] = (int) ceil(s->need[i][j] * ((double)rand())/ (double)RAND_MAX);
 			sum += request[j];
 		}
 	}
-	printf("Process %d: Requesting resources.\n", i);
-	pthread_mutex_unlock(&state_mutex);
+	printf("Process %d: Requesting resources.\n",i);
 }
 
 /* Generate a release vector */
 void generate_release(int i, int *request) {
-	pthread_mutex_lock(&state_mutex);
 	int j, sum = 0;
 	while (!sum) {
-		for (j = 0; j < n; j++) {
-			request[j] = s->allocation[i][j] * ((double) rand()) / (double) RAND_MAX;
+		for (j = 0;j < n; j++) {
+			request[j] = (int) ceil(s->allocation[i][j] * ((double)rand())/ (double)RAND_MAX);
 			sum += request[j];
 		}
 	}
-	printf("Process %d: Releasing resources.\n", i);
-	pthread_mutex_unlock(&state_mutex);
+	printf("Process %d: Releasing resources.\n",i);
+}
+
+int is_need_empty(int i) {
+	int j;
+	for (j = 0; j < n; j++) {
+		if (s->need[i][j])
+			return 0;
+	}
+	return 1;
 }
 
 /* Threads starts here */
 void *process_thread(void *param) {
 	/* Process number */
-	int i = (int) (long) param, j;
+	int i = (int) (long) param;
 	/* Allocate request vector */
 	int *request = malloc(n * sizeof(int));
-	int iterations = 0;
 
-	while (iterations < 1000) {
+	while (!is_need_empty(i)) {
 		/* Generate request */
 		generate_request(i, request);
 		while (!resource_request(i, request)) {
+			printf("Request %d waiting\n", i);
 			/* Wait */
 			Sleep(100);
 		}
@@ -176,12 +207,13 @@ void *process_thread(void *param) {
 		generate_release(i, request);
 		/* Release resources */
 		resource_release(i, request);
+		printf("Process %d: Resources released.\n", i);
 		/* Wait */
 		Sleep(1000);
-
-		iterations++;
 	}
 	free(request);
+	printf("Process %d finished\n", i);
+	return NULL;
 }
 
 State *allocate_state() {
@@ -283,10 +315,13 @@ int main(int argc, char *argv[]) {
 	printf("\n");
 
 	/* If initial state is unsafe then terminate with error */
-
+	if (!is_state_safe(s)) {
+		printf("Initial state is not safe!\n");
+		exit(40);
+	}
 
 	/* Create m threads */
-	pthread_t tid[m];
+	pthread_t *tid = malloc(m*sizeof(pthread_t));
 	for (i = 0; i < m; i++)
 		pthread_create(tid + i, NULL, process_thread, (void *) (long) i);
 
@@ -294,7 +329,11 @@ int main(int argc, char *argv[]) {
 	for (i = 0; i < m; i++)
 		pthread_join(tid[i], NULL);
 
+	free(tid);
+
 	/* Free state memory */
 	free_state(s);
+
+	return 0;
 }
 
